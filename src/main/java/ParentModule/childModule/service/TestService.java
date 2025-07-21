c94ace605d0f860a93711dcb3112c3b06a2c2950
Biswajeet Tripathy <biswajeet.10829773@ltimindtree.com>
2025-07-21 12:23:11 +0530

    Adding test service,

---
+++ src/main/java/ParentModule/childModule/service/TestService.java
@@ -0,0 +1,13 @@
+package ParentModule.childModule.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String testMessage()
+    {
+        return "Hello";
+    }
+
+}
